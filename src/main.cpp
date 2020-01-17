3269154a8d452e95f9a5207b13a6d4c1b626d614
dilinipp <59647107+dilinipp@users.noreply.github.com>
2020-01-17 12:33:15 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,7 @@
 #define USE_SERIAL Serial
 
 #define CURRENT_VERSION VERSION
-#define CLOUD_FUNCTION_URL "http://us-central1-gcloud-ota-update.cloudfunctions.net/getDownloadUrl"
+#define CLOUD_FUNCTION_URL "https://asia-east2-orelhome-producttest-258407.cloudfunctions.net/getDownloadUrl_dilini"
 
 WiFiClient client;
 #if defined(ESP8266)
